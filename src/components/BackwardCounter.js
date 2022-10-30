9515f0661723827f7ce589b42aa4bd827a55fb78
vibesx <lucian.paslariu@gmail.com>
2022-10-30 16:36:11 +0200

    added a custom hook and replaced logic in both components

--- src/components/BackwardCounter.js
+++ src/components/BackwardCounter.js
@@ -1,19 +1,14 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
+import useCounter from "../hooks/use-counter";
 
-import Card from './Card';
+import Card from "./Card";
 
 const BackwardCounter = () => {
-  const [counter, setCounter] = useState(0);
+	// another option for arguments for useCounter is to pass a function and just call it inside useCounter
+	// ex: useCounter((prevCounter) => {setCounter((prevCounter) => prevCounter + 1);} or similar)
+	const counter = useCounter(false);
 
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setCounter((prevCounter) => prevCounter - 1);
-    }, 1000);
-
-    return () => clearInterval(interval);
-  }, []);
-
-  return <Card>{counter}</Card>;
+	return <Card>{counter}</Card>;
 };
 
 export default BackwardCounter;
